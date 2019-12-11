9d7971fae97878886f4ab512046f2ccec805046c
Tiago Geanezini <t.geanezini@gmail.com>
2019-12-10 23:10:18 -0300

    feat(adapter): Added list adapter

---
+++ app/src/main/java/com/tgeanezini/mobile/codehero/CharactersAdapter.kt
@@ -0,0 +1,35 @@
+package com.tgeanezini.mobile.codehero
+
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item_character.view.*
+
+class CharactersAdapter (private val characters: List<Character>) :
+    RecyclerView.Adapter<CharactersAdapter.CharactersViewHolder>() {
+
+    class CharactersViewHolder(characterView: View) : RecyclerView.ViewHolder(characterView) {
+        val name = characterView.characterName
+        val image = characterView.characterImage
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharactersViewHolder {
+        val characterView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_character, parent, false)
+
+        return CharactersViewHolder(characterView)
+    }
+
+    override fun getItemCount(): Int {
+        return characters.size
+    }
+
+    override fun onBindViewHolder(holder: CharactersViewHolder, position: Int) {
+        val character = characters[position]
+        holder.image.setImageURI(Uri.parse(character.thumbnail.path))
+        holder.name.text = character.name
+    }
+
+}
--- app/src/main/java/com/tgeanezini/mobile/codehero/MainActivity.kt
+++ app/src/main/java/com/tgeanezini/mobile/codehero/MainActivity.kt
@@ -5,12 +5,24 @@ import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //viewManager = LinearLayoutManager(this)
+        //viewAdapter = CharactersAdapter()
+
+        recyclerView = findViewById<RecyclerView>(R.id.charactersList).apply {
+            setHasFixedSize(true)
+        }
     }
 
     private fun checkInternetConnection(context: Context): Boolean {
