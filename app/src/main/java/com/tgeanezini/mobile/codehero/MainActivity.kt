547ec08d01c6a3f21081d5e547506b8f75eb111d
Tiago Geanezini <tiago.geanezini@inlog.com.br>
2019-12-12 11:30:00 -0300

    feat(search): Implemented hero search funtion

--- app/src/main/java/com/tgeanezini/mobile/codehero/MainActivity.kt
+++ app/src/main/java/com/tgeanezini/mobile/codehero/MainActivity.kt
@@ -6,12 +6,15 @@ import android.net.NetworkInfo
 import android.os.Bundle
 import android.view.View
 import android.app.AlertDialog
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_characters.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,6 +24,8 @@ import java.security.MessageDigest
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var remoteConfig: FirebaseRemoteConfig
+    lateinit var characters: List<Character>
+    lateinit var adapter: CharactersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +38,50 @@ class MainActivity : AppCompatActivity() {
             .build()
         remoteConfig.setConfigSettingsAsync(remoteConfigSettings)
 
-        fetchConfigs()
+        if (checkInternetConnection(this)) {
+            fetchConfigs()
+        } else {
+            val dialogBuilder = AlertDialog.Builder(this)
+            dialogBuilder.setMessage(getString(R.string.dialog_falha_message))
+                .setPositiveButton("OK") { dialog,_ ->
+                    dialog.cancel()
+                    finish()
+                }
+
+            val dialog = dialogBuilder.create()
+            dialog.setTitle(getString(R.string.dialog_falha_title))
+            dialog.show()
+        }
+
+        characterSearch.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                // Método não utilizado
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Método não utilizado
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val query = s.toString().toLowerCase().trim()
+                val filteredList = ArrayList<Character>()
+
+                for (character in characters) {
+                    val characterName = character.name.toLowerCase()
+                    if (characterName.contains(query)) {
+                        filteredList.add(character)
+                    }
+                }
+
+                adapter = CharactersAdapter(filteredList, applicationContext)
+                recyclerView = findViewById<RecyclerView>(R.id.charactersList).apply {
+                    setHasFixedSize(true)
+                    layoutManager = LinearLayoutManager(applicationContext)
+                }
+                recyclerView.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+        })
     }
 
     private fun checkInternetConnection(context: Context): Boolean {
@@ -77,7 +125,7 @@ class MainActivity : AppCompatActivity() {
                     charactersLoading.visibility = View.GONE
                     content.visibility = View.VISIBLE
 
-                    val characters = it.data.results
+                    characters = it.data.results
 
                     recyclerView = findViewById<RecyclerView>(R.id.charactersList).apply {
                         setHasFixedSize(true)
